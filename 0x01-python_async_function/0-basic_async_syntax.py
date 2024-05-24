897c10e31942d6b8f94fd372bb8fbf4b6a662851
Henshaw <josephhensh2000@gmail.com>
2024-05-24 07:21:16 -0700

    Update: Annotate variables

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -4,7 +4,7 @@ import asyncio
 import random
 
 
-async def wait_random(max_delay: int = 10):
+async def wait_random(max_delay: int = 10) -> float:
     """Asynchronous coroutine waits for a random delay between 0 and
     `max_delay` seconds and returns it"""
     delay = random.uniform(0, max_delay)
