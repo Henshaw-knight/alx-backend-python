dbaed8319a11e26bece5a5cd138e2f19c5d64c4a
Henshaw <josephhensh2000@gmail.com>
2024-05-25 09:44:32 -0700

    Annontate function and validate style

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,9 +1,11 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """async_generator coroutine module"""
 import asyncio
 import random
+from typing import AsyncGenerator
 
-async def async_generator():
+
+async def async_generator() -> AsyncGenerator[float, None]:
     """Yields a random number between 0 and 10"""
     for i in range(10):
         await asyncio.sleep(1)
