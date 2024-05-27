82766e9759d940632840463b11fd97ff3d7e5ff2
Henshaw <josephhensh2000@gmail.com>
2024-05-27 06:32:37 -0700

    Update file to pass time check

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -9,13 +9,7 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 async def measure_runtime() -> float:
     """Measures and returns the total runtime"""
     start_time = time.time()
-    results = (await asyncio.gather(
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension()
-        )
-    )
+    results = await asyncio.gather(*(async_comprehension() for i in range(4)))
     end_time = time.time()
 
     return (end_time - start_time)
